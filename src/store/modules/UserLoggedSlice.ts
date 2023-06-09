90826dd91b96dabe22a102052fd8283e63da2b7d
Diego G.Lins <dl7designer@gmail.com>
2023-06-09 10:43:30 -0300

    alteracao no slice de usuario logado

--- src/store/modules/UserLoggedSlice.ts
+++ src/store/modules/UserLoggedSlice.ts
@@ -1,22 +1,13 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import UserRegisterType from '../../types/UserRegisterType';
-import ErrandsType from '../../types/ErrandsType';
+import { createSlice } from '@reduxjs/toolkit';
 
-const initialState: UserRegisterType = {
-  email: '',
-  password: '',
-  errands: []
-};
+const initialState = '';
 
 const userLoggedSlice = createSlice({
   name: 'userLogged',
   initialState,
   reducers: {
-    loggeds: (_, action: PayloadAction<UserRegisterType>) => {
+    userLogged(state, action) {
       return action.payload;
-    },
-    add: (state, action: PayloadAction<ErrandsType>) => {
-      state.errands.push(action.payload);
     },
     logout() {
       return initialState;
@@ -24,5 +15,5 @@ const userLoggedSlice = createSlice({
   }
 });
 
-export const {logout, loggeds, add } = userLoggedSlice.actions;
+export const { logout, userLogged } = userLoggedSlice.actions;
 export default userLoggedSlice.reducer;
