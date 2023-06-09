e986896740879e4a284dfe1fe24d0ac2461c0324
Diego G.Lins <dl7designer@gmail.com>
2023-06-09 10:50:07 -0300

    arquivo alterado

--- src/types/UserRegisterType.ts
+++
@@ -1,9 +0,0 @@
-import ErrandsType from './ErrandsType';
-
-interface UserRegisterType {
-    email: string;
-    password: string;
-    errands: ErrandsType[],
-}
-export default UserRegisterType;
-
